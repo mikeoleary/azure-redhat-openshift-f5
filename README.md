a390e8192cd06da85f33b2b0b36be65baa141c21
Ubuntu <michael.w.oleary@gmail.com>
2020-09-14 17:08:49 +0000

    updates for deployment script - reduced number of arguments - minor fix to wget commands - minor fix to cis.yaml

--- README.md
+++ README.md
@@ -3,16 +3,17 @@
 ## Deploy
   [![Deploy to Azure](http://azuredeploy.net/deploybutton.png)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fmikeoleary%2Fazure-redhat-openshift-f5%2Fmain%2Fdeploy.json)
 
+## Pre-requisits
+1. **You will need a Service Principal in Azure with Contributor permissions on the Subscription.** This is because this template provides a Service Principal to the OCP cluster, which then requires permissions to create a Resource Group in the subscription.
+
 ## Instructions
 For the official instructions from Microsoft on deploying ARO, you can view [this tutorial](https://docs.microsoft.com/en-us/azure/openshift/tutorial-create-cluster). These instructions are intended to be run from a Linux workstation with [az cli](https://docs.microsoft.com/en-us/cli/azure/install-azure-cli) installed. Instructions for deploying this demo environment with F5 via ARM template are below:
 1. Deploy ARM template by clicking the Deploy button above.
 2. Optionally, further configure F5 and OpenShift environment by accessing the environment via the URL's in the deployment outputs.
 
-
-
 ## Pledge for Racial Equality, Diversity, and Inclusion
-I do not represent F5 and the code in this repo is my own, but I do work for F5. F5 has [pledged](https://www.f5.com/company/blog/our-pledge-for-racial-equality--diversity--and-inclusion) to fight against racism, and I have joined that pledge. Part of this effort includes updating our code and documentation to discontinue the use of terms that may be considered racially charged. 
-
+I do not represent F5 and the code in this repo is my own, but I do work for F5. F5 has [pledged](https://www.f5.com/company/blog/our-pledge-for-racial-equality--diversity--and-inclusion) to fight against racism, and I have joined that pledge. Part of this effort includes updating our code and documentation to discontinue the use of terms that may be considered racially charged.  
+  
 To that end, this repo has removed, where possible, words such as "master" and "blacklist" and replaced them with "main" (eg, the default git branch), or "primary" (eg, the subnet name), or "denylist" (not used in this repo at the time of this writing). If you see any terms considered racially charged, please submit an issue to bring it to attention. This effort is expected to be on-going and faces some challenges (eg, hardcoded protocol terms) but over time the intent is to remove all terms that are considered racially charged. Thank you for any help in this regard.
 
 ## Support and Issues
--- scripts/deploymentScript.sh
+++ scripts/deploymentScript.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 ## THIS SCRIPT WILL CONFIGURE THE OCP CLUSTER AND BIG-IP TO PREPARE FOR CIS INTEGRATION VIA VXLAN OVERLAY
-BIGIP_INT_SELFIP=$1
-ARO_API_SERVER=$2
-ARO_KUBE_ADMIN=$3
-ARO_PASSWORD=$4
-BIGIP_MGMT_PUBLIC_IP=$5
-BIGIP_MGMT_PRIVATE_IP=$6
-BIGIP_MGMT_USERNAME=$7
-BIGIP_MGMT_PASSWORD=$8
+ARO_API_SERVER=$1
+ARO_PASSWORD=$2
+BIGIP_MGMT_ADDRESS=$3
+BIGIP_MGMT_PASSWORD=$4
 
 ##Download OC client tool
 wget https://raw.githubusercontent.com/mikeoleary/azure-redhat-openshift-f5/main/client/openshift-client-linux.tar.gz -O /tmp/openshift-client-linux.tar.gz
@@ -16,10 +12,21 @@ tar -xf /tmp/openshift-client-linux.tar.gz -C /tmp
 mv /tmp/oc /usr/local/bin -f
 mv /tmp/kubectl /usr/local/bin -f
 
+#get BIGIP IP addresses needed by CIS
+#Get private IP for Internal SelfIP
+BIGIP_INT_SELFIP_WITH_MASK=$(curl -s -k -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/self/self_3nic | jq .address -r)
+read -a strarr <<< $(echo $BIGIP_INT_SELFIP_WITH_MASK | tr "/" " ")
+BIGIP_INT_SELFIP=${strarr[0]}
+#Get private IP for Mgmt 
+BIGIP_MGMT_PRIVATE_IP_WITH_MASK=$(curl -s -k -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/sys/management-ip | jq .items[0].fullPath -r)
+read -a strarr <<< $(echo $BIGIP_MGMT_PRIVATE_IP_WITH_MASK | tr "/" " ")
+BIGIP_MGMT_PRIVATE_IP=${strarr[0]}
+
+
 ##Download and edit yaml file template for hostsubnet, then apply new hostsubnet
 wget https://raw.githubusercontent.com/mikeoleary/azure-redhat-openshift-f5/main/templates/f5-bigip-node01.yaml  -O /tmp/f5-bigip-node01.yaml
 sed -i -e "s/INTERNALSELFIP/$BIGIP_INT_SELFIP/" /tmp/f5-bigip-node01.yaml
-oc login $ARO_API_SERVER -u=$ARO_KUBE_ADMIN -p=$ARO_PASSWORD
+oc login $ARO_API_SERVER -u=kubeadmin -p=$ARO_PASSWORD
 oc apply -f /tmp/f5-bigip-node01.yaml
 
 #get cluster network mask. This is usually /14
@@ -35,18 +42,19 @@ BIGIP_TUNNEL_SELFIP="${BIGIP_HOST_SUBNET%.*}.100/$CLUSTERNETWORK_MASK"
 
 ###Connect to BIG-IP and create tunnel profile, tunnel, selfIP on tunnel, and add vxlan port 4789 to internal self IP
 #Create multipoint vxlan profile
-curl -s -k -X POST -H "Content-Type: application/json" -u $BIGIP_MGMT_USERNAME:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_PUBLIC_IP/mgmt/tm/net/tunnels/vxlan -d '{"name": "vxlan-mp", "floodingType": "multipoint" }'
+curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/tunnels/vxlan -d '{"name": "vxlan-mp", "floodingType": "multipoint" }'
 #Create vxlan tunnel based on vxlan profile
-curl -s -k -X POST -H "Content-Type: application/json" -u $BIGIP_MGMT_USERNAME:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_PUBLIC_IP/mgmt/tm/net/tunnels/tunnel -d '{"name": "openshift_vxlan", "key": "0", "profile": "vxlan-mp", "localAddress": "'"$BIGIP_INT_SELFIP"'" }'
+curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/tunnels/tunnel -d '{"name": "openshift_vxlan", "key": "0", "profile": "vxlan-mp", "localAddress": "'"$BIGIP_INT_SELFIP"'" }'
 #Create selfIP on tunnel
-curl -s -k -X POST -H "Content-Type: application/json" -u $BIGIP_MGMT_USERNAME:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_PUBLIC_IP/mgmt/tm/net/self -d '{"name": "tunnelSelfIP", "address": "'"$BIGIP_TUNNEL_SELFIP"'", "vlan": "openshift_vxlan", "profile": "vxlan-mp", "localAddress": "'"$BIGIP_INT_SELFIP"'", "allowService": "all" }'
+curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/self -d '{"name": "tunnelSelfIP", "address": "'"$BIGIP_TUNNEL_SELFIP"'", "vlan": "openshift_vxlan", "profile": "vxlan-mp", "localAddress": "'"$BIGIP_INT_SELFIP"'", "allowService": "all" }'
 #set allowed services on selfIP of Internal VLAN to be default + tcp:4789
-curl -s -k -X PATCH -H "Content-Type: application/json" -u $BIGIP_MGMT_USERNAME:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_PUBLIC_IP/mgmt/tm/net/self/self_3nic -d '{"allowService": ["default","tcp:4789"]}'
+curl -s -k -X PATCH -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/self/self_3nic -d '{"allowService": ["default","tcp:4789"]}'
 
 ##Deploy F5 CIS
 rm -r /tmp/cis -f
 mkdir /tmp/cis
-wget https://raw.githubusercontent.com/mikeoleary/azure-redhat-openshift-f5/main/cis/cis.yaml /tmp/cis
-sed -i -e "s/<base64password>/$BIGIP_MGMT_PASSWORD/" /tmp/cis/cis.yaml
+wget https://raw.githubusercontent.com/mikeoleary/azure-redhat-openshift-f5/main/cis/cis.yaml -O /tmp/cis.yaml
+$BIGIP_MGMT_PASSWORD_BASE64=$(echo  $BIGIP_MGMT_PASSWORD | base64)
+sed -i -e "s/<base64password>/$BIGIP_MGMT_PASSWORD_BASE64/" /tmp/cis/cis.yaml
 sed -i -e "s/<bigipUrl>/$BIGIP_MGMT_PRIVATE_IP/" /tmp/cis/cis.yaml
 oc apply -f /tmp/cis.yaml
