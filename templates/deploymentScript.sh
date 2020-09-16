a3009d49c5851ebe8de72df0a4c0fca71b3cd426
Ubuntu <michael.w.oleary@gmail.com>
2020-09-16 14:05:43 +0000

    Updates for post deploy config - minor changes to deploymentScript to support f5 routes - addition of nested template applies NSG to ext subnet -lb rules added allowing 80,443 thru ALB - BIGIP password passed as secure env variable for container script - minor perf improvement in 1 less azcli call to Azure in script - script outputs values for user to access OCP cluster post deployment

--- templates/deploymentScript.sh
+++ templates/deploymentScript.sh
@@ -4,21 +4,23 @@
 ARO_CLUSTER_NAME=$1
 ARO_CLUSTER_RESOURCE_GROUP=$2
 BIGIP_MGMT_ADDRESS=$3
-BIGIP_MGMT_PASSWORD=$4
-BIGIP_VSERVER_ADDR=$5
-MY_CUSTOM_DOMAIN=$6
+BIGIP_VSERVER_ADDR=$4
+MY_CUSTOM_DOMAIN=$5
 
 #Get ARO cluster details, and password
-ARO_PASSWORD=$(az aro list-credentials --name $ARO_CLUSTER_NAME --resource-group $ARO_CLUSTER_RESOURCE_GROUP | jq .kubeadminPassword -r)
+ARO_CREDENTIALS=$(az aro list-credentials --name $ARO_CLUSTER_NAME --resource-group $ARO_CLUSTER_RESOURCE_GROUP)
+ARO_USERNAME=$(echo $ARO_CREDENTIALS | jq .kubeadminUsername -r)
+ARO_PASSWORD=$(echo $ARO_CREDENTIALS | jq .kubeadminPassword -r)
 ARO_CONFIG=$(az aro show --name $ARO_CLUSTER_NAME --resource-group $ARO_CLUSTER_RESOURCE_GROUP)
 #Extract the subnets and API and Console addresses of ARO cluster
 ARO_API_SERVER=$(echo $ARO_CONFIG | jq .apiserverProfile.url -r) 
 ARO_CONSOLE_URL=$(echo $ARO_CONFIG | jq .consoleProfile.url -r) 
 ARO_MASTER_SUBNET_ID=$(echo $ARO_CONFIG | jq .masterProfile.subnetId -r)
 ARO_WORKER_SUBNET_ID=$(echo $ARO_CONFIG | jq .workerProfiles[0].subnetId -r)
-#Get the subnet CIDR blocks of ARO masters and workers
-MASTER_SUBNET_IP_CIDR=$(az network vnet subnet show --ids $ARO_MASTER_SUBNET_ID | jq .addressPrefix -r)
-WORKER_SUBNET_IP_CIDR=$(az network vnet subnet show --ids $ARO_WORKER_SUBNET_ID | jq .addressPrefix -r)
+#Get the subnet CIDR blocks of ARO master subnet and worker subnet from Azure
+ARO_SUBNET_IDS_ARRAY=$(az network vnet subnet show --ids $ARO_MASTER_SUBNET_ID $ARO_WORKER_SUBNET_ID)
+MASTER_SUBNET_IP_CIDR=$(echo $ARO_SUBNET_IDS_ARRAY  | jq '.[]  | select(.id == "'"$ARO_MASTER_SUBNET_ID"'") | .addressPrefix' -r)
+WORKER_SUBNET_IP_CIDR=$(echo $ARO_SUBNET_IDS_ARRAY  | jq '.[]  | select(.id == "'"$ARO_WORKER_SUBNET_ID"'") | .addressPrefix' -r)
 
 ##Download OC client tool
 wget https://raw.githubusercontent.com/mikeoleary/azure-redhat-openshift-f5/main/client/openshift-client-linux.tar.gz -O /tmp/openshift-client-linux.tar.gz
@@ -31,11 +33,12 @@ mv /tmp/kubectl /usr/local/bin -f
 BIGIP_INT_SELFIP_WITH_MASK=$(curl -s -k -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/self/self_3nic | jq .address -r)
 read -a strarr <<< $(echo $BIGIP_INT_SELFIP_WITH_MASK | tr "/" " ")
 BIGIP_INT_SELFIP=${strarr[0]}
+#Get Internal vlan gw
+BIGIP_INT_GW="${BIGIP_INT_SELFIP%.*}.1"
 #Get private IP for Mgmt 
 BIGIP_MGMT_PRIVATE_IP_WITH_MASK=$(curl -s -k -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/sys/management-ip | jq .items[0].fullPath -r)
 read -a strarr <<< $(echo $BIGIP_MGMT_PRIVATE_IP_WITH_MASK | tr "/" " ")
 BIGIP_MGMT_PRIVATE_IP=${strarr[0]}
-
 
 ##Download and edit yaml file template for hostsubnet, then apply new hostsubnet
 wget https://raw.githubusercontent.com/mikeoleary/azure-redhat-openshift-f5/main/templates/f5-bigip-node01.yaml  -O /tmp/f5-bigip-node01.yaml
@@ -64,8 +67,8 @@ curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASS
 #set allowed services on selfIP of Internal VLAN to be default + tcp:4789
 curl -s -k -X PATCH -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/self/self_3nic -d '{"allowService": ["default","tcp:4789"]}'
 #Create routes on BIG-IP so that traffic to the Master and Worker nodes traverses the internal NIC
-curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/route/ -d '{"name": "routeMasterNodes", "partition": "Common", "gw": "'"$BIGIP_INT_SELFIP"'", "network": "'"$MASTER_SUBNET_IP_CIDR"'"}'
-curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/route/ -d '{"name": "routeWorkerNodes", "partition": "Common", "gw": "'"$BIGIP_INT_SELFIP"'", "network": "'"$WORKER_SUBNET_IP_CIDR"'"}'
+curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/route/ -d '{"name": "routeMasterNodes", "partition": "Common", "gw": "'"$BIGIP_INT_GW"'", "network": "'"$MASTER_SUBNET_IP_CIDR"'"}'
+curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/route/ -d '{"name": "routeWorkerNodes", "partition": "Common", "gw": "'"$BIGIP_INT_GW"'", "network": "'"$WORKER_SUBNET_IP_CIDR"'"}'
 
 ##Deploy demoapp
 rm /tmp/demoapp.yaml -f
@@ -81,3 +84,7 @@ sed -i -e "s/<base64password>/$BIGIP_MGMT_PASSWORD_BASE64/" /tmp/cis.yaml
 sed -i -e "s/<bigipUrl>/$BIGIP_MGMT_PRIVATE_IP/" /tmp/cis.yaml
 sed -i -e "s/<vserver-addr>/$BIGIP_VSERVER_ADDR/" /tmp/cis.yaml
 oc apply -f /tmp/cis.yaml
+
+##Output values from script to the default value for output using built in environment variable AZ_SCRIPTS_OUTPUT_PATH
+JSON_OUTPUT='{"aro_api_server": "'"$ARO_API_SERVER"'", "aro_console_url": "'"$ARO_CONSOLE_URL"'", "aro_username": "'"$ARO_USERNAME"'", "aro_password": "'"$ARO_PASSWORD"'"}'
+echo $JSON_OUTPUT > $AZ_SCRIPTS_OUTPUT_PATH
