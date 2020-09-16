2302b87890089aa65f7a2df1918a7e5077296102
Ubuntu <michael.w.oleary@gmail.com>
2020-09-16 00:33:52 +0000

    updates for Deployment Script improvements - f5 template now outputs VIP IP addresses and LB IP address. - cis.yaml now includes route-vserver-addr value - demoapp added for OCP installation - new input param for custom domain name - new argument passed to deploymentScript - updates to deployment script to configure Routes on BIGIP - updates to deployment script to deploy demo app

--- templates/deploymentScript.sh
+++ templates/deploymentScript.sh
@@ -5,10 +5,20 @@ ARO_CLUSTER_NAME=$1
 ARO_CLUSTER_RESOURCE_GROUP=$2
 BIGIP_MGMT_ADDRESS=$3
 BIGIP_MGMT_PASSWORD=$4
+BIGIP_VSERVER_ADDR=$5
+MY_CUSTOM_DOMAIN=$6
 
-#Get ARO cluster API server and password
-ARO_API_SERVER=$(az aro show --name $ARO_CLUSTER_NAME --resource-group $ARO_CLUSTER_RESOURCE_GROUP --query apiserverProfile.url -o tsv)
+#Get ARO cluster details, and password
 ARO_PASSWORD=$(az aro list-credentials --name $ARO_CLUSTER_NAME --resource-group $ARO_CLUSTER_RESOURCE_GROUP | jq .kubeadminPassword -r)
+ARO_CONFIG=$(az aro show --name $ARO_CLUSTER_NAME --resource-group $ARO_CLUSTER_RESOURCE_GROUP)
+#Extract the subnets and API and Console addresses of ARO cluster
+ARO_API_SERVER=$(echo $ARO_CONFIG | jq .apiserverProfile.url -r) 
+ARO_CONSOLE_URL=$(echo $ARO_CONFIG | jq .consoleProfile.url -r) 
+ARO_MASTER_SUBNET_ID=$(echo $ARO_CONFIG | jq .masterProfile.subnetId -r)
+ARO_WORKER_SUBNET_ID=$(echo $ARO_CONFIG | jq .workerProfiles[0].subnetId -r)
+#Get the subnet CIDR blocks of ARO masters and workers
+MASTER_SUBNET_IP_CIDR=$(az network vnet subnet show --ids $ARO_MASTER_SUBNET_ID | jq .addressPrefix -r)
+WORKER_SUBNET_IP_CIDR=$(az network vnet subnet show --ids $ARO_WORKER_SUBNET_ID | jq .addressPrefix -r)
 
 ##Download OC client tool
 wget https://raw.githubusercontent.com/mikeoleary/azure-redhat-openshift-f5/main/client/openshift-client-linux.tar.gz -O /tmp/openshift-client-linux.tar.gz
@@ -44,7 +54,7 @@ read -a strarr <<< $(echo $BIGIP_HOST_SUBNET_WITH_MASK | tr "/" " ")
 BIGIP_HOST_SUBNET=${strarr[0]}
 BIGIP_TUNNEL_SELFIP="${BIGIP_HOST_SUBNET%.*}.100/$CLUSTERNETWORK_MASK"
 
-###Connect to BIG-IP and create tunnel profile, tunnel, selfIP on tunnel, and add vxlan port 4789 to internal self IP
+###Connect to BIG-IP and create tunnel profile, tunnel, selfIP on tunnel, and add vxlan port 4789 to internal self IP. Also, create a route so that the openshift worker and master nodes are reached via BIG-IP internal NIC.
 #Create multipoint vxlan profile
 curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/tunnels/vxlan -d '{"name": "vxlan-mp", "floodingType": "multipoint" }'
 #Create vxlan tunnel based on vxlan profile
@@ -53,12 +63,21 @@ curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASS
 curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/self -d '{"name": "tunnelSelfIP", "address": "'"$BIGIP_TUNNEL_SELFIP"'", "vlan": "openshift_vxlan", "profile": "vxlan-mp", "localAddress": "'"$BIGIP_INT_SELFIP"'", "allowService": "all" }'
 #set allowed services on selfIP of Internal VLAN to be default + tcp:4789
 curl -s -k -X PATCH -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/self/self_3nic -d '{"allowService": ["default","tcp:4789"]}'
+#Create routes on BIG-IP so that traffic to the Master and Worker nodes traverses the internal NIC
+curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/route/ -d '{"name": "routeMasterNodes", "partition": "Common", "gw": "'"$BIGIP_INT_SELFIP"'", "network": "'"$MASTER_SUBNET_IP_CIDR"'"}'
+curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/route/ -d '{"name": "routeWorkerNodes", "partition": "Common", "gw": "'"$BIGIP_INT_SELFIP"'", "network": "'"$WORKER_SUBNET_IP_CIDR"'"}'
+
+##Deploy demoapp
+rm /tmp/demoapp.yaml -f
+wget https://raw.githubusercontent.com/mikeoleary/azure-redhat-openshift-f5/main/demoapp/demoapp.yaml -O /tmp/demoapp.yaml
+sed -i -e "s/<my_custom_domain>/$MY_CUSTOM_DOMAIN/" /tmp/demoapp.yaml
+oc apply -f /tmp/demoapp.yaml
 
 ##Deploy F5 CIS
-rm -r /tmp/cis -f
-mkdir /tmp/cis
+rm /tmp/cis.yaml -f
 wget https://raw.githubusercontent.com/mikeoleary/azure-redhat-openshift-f5/main/cis/cis.yaml -O /tmp/cis.yaml
 BIGIP_MGMT_PASSWORD_BASE64=$(echo -n  $BIGIP_MGMT_PASSWORD | base64)
 sed -i -e "s/<base64password>/$BIGIP_MGMT_PASSWORD_BASE64/" /tmp/cis.yaml
 sed -i -e "s/<bigipUrl>/$BIGIP_MGMT_PRIVATE_IP/" /tmp/cis.yaml
+sed -i -e "s/<vserver-addr>/$BIGIP_VSERVER_ADDR/" /tmp/cis.yaml
 oc apply -f /tmp/cis.yaml
