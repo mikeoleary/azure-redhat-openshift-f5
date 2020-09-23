83b4deedb069f479a4af65ae1fb6d8a1b70a7639
Ubuntu <ubuntu@ip-10-0-0-36.ec2.internal>
2020-09-23 00:26:22 +0000

    recreating files

--- templates/deploymentScriptDeviceA.sh
+++ templates/deploymentScriptDeviceA.sh
@@ -5,7 +5,8 @@ ARO_CLUSTER_NAME=$1
 ARO_CLUSTER_RESOURCE_GROUP=$2
 BIGIP_MGMT_ADDRESS=$3
 BIGIP_VSERVER_ADDR=$4
-MY_CUSTOM_DOMAIN=$5
+BIGIP_INT_FLOAT_IP=$5
+MY_CUSTOM_DOMAIN=$6
 
 #Get ARO cluster details, and password
 ARO_CREDENTIALS=$(az aro list-credentials --name $ARO_CLUSTER_NAME --resource-group $ARO_CLUSTER_RESOURCE_GROUP)
@@ -28,11 +29,14 @@ tar -xf /tmp/openshift-client-linux.tar.gz -C /tmp
 mv /tmp/oc /usr/local/bin -f
 mv /tmp/kubectl /usr/local/bin -f
 
-#get BIGIP IP addresses needed by CIS
 #Get private IP for Internal SelfIP
 BIGIP_INT_SELFIP_WITH_MASK=$(curl -s -k -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/self/self_3nic | jq .address -r)
 read -a strarr <<< $(echo $BIGIP_INT_SELFIP_WITH_MASK | tr "/" " ")
 BIGIP_INT_SELFIP=${strarr[0]}
+BIGIP_INT_SELFIP_MASK=${strarr[1]}
+#set BIGIP internal float SelfIP
+BIGIP_INT_FLOAT_IP_WITH_MASK=$BIGIP_INT_FLOAT_IP"/"$BIGIP_INT_SELFIP_MASK
+curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/self -d '{"name": "internalSelfFloat", "address": "'"$BIGIP_INT_FLOAT_IP_WITH_MASK"'", "vlan": "internal", "traffic-group":"traffic-group-1", "allowService": ["default", "tcp:4789"] }'
 #Get Internal vlan gw
 BIGIP_INT_GW="${BIGIP_INT_SELFIP%.*}.1"
 #Get private IP for Mgmt 
@@ -41,10 +45,17 @@ read -a strarr <<< $(echo $BIGIP_MGMT_PRIVATE_IP_WITH_MASK | tr "/" " ")
 BIGIP_MGMT_PRIVATE_IP=${strarr[0]}
 
 ##Download and edit yaml file template for hostsubnet, then apply new hostsubnet
-wget https://raw.githubusercontent.com/mikeoleary/azure-redhat-openshift-f5/main/templates/f5-bigip-node01.yaml  -O /tmp/f5-bigip-node01.yaml
-sed -i -e "s/INTERNALSELFIP/$BIGIP_INT_SELFIP/" /tmp/f5-bigip-node01.yaml
+wget https://raw.githubusercontent.com/mikeoleary/azure-redhat-openshift-f5/main/templates/f5-bigip-node.yaml  -O /tmp/f5-bigip-node.yaml
+cp /tmp/f5-bigip-node.yaml /tmp/f5-bigip-node-float.yaml
+sed -i -e "s/<INTERNALSELFIP>/$BIGIP_INT_SELFIP/" /tmp/f5-bigip-node.yaml
+sed -i -e "s/<NODENAME>/f5-bigip-node01/" /tmp/f5-bigip-node.yaml
 oc login $ARO_API_SERVER -u=kubeadmin -p=$ARO_PASSWORD
-oc apply -f /tmp/f5-bigip-node01.yaml
+oc apply -f /tmp/f5-bigip-node.yaml
+sed -i -e "s/<INTERNALSELFIP>/$BIGIP_INT_FLOAT_IP/" /tmp/f5-bigip-node-float.yaml
+sed -i -e "s/<NODENAME>/f5-bigip-float/" /tmp/f5-bigip-node-float.yaml
+oc apply -f /tmp/f5-bigip-node-float.yaml
+rm -f /tmp/f5-bigip-node.yaml
+rm -f /tmp/f5-bigip-node-float.yaml
 
 #get cluster network mask. This is usually /14
 CLUSTERNETWORK=$(oc get clusternetwork -o json | jq .items[0].network -r)
@@ -52,7 +63,7 @@ read -a strarr <<< $(echo $CLUSTERNETWORK | tr "/" " ")
 CLUSTERNETWORK_MASK=${strarr[1]}
 
 #Calculate a self IP for tunnel on BIGIP. First get assigned host subnet, which is usually a /23 range like 10.130.2.0/23. We will assign .100 from this range, eg, 10.130.2.100
-BIGIP_HOST_SUBNET_WITH_MASK=$(oc get hostsubnet f5-bigip-node01 -o json | jq .subnet -r)
+BIGIP_HOST_SUBNET_WITH_MASK=$(oc get hostsubnet f5-bigip-float -o json | jq .subnet -r)
 read -a strarr <<< $(echo $BIGIP_HOST_SUBNET_WITH_MASK | tr "/" " ")
 BIGIP_HOST_SUBNET=${strarr[0]}
 BIGIP_TUNNEL_SELFIP="${BIGIP_HOST_SUBNET%.*}.100/$CLUSTERNETWORK_MASK"
@@ -65,7 +76,7 @@ curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASS
 #Create selfIP on tunnel
 curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/self -d '{"name": "tunnelSelfIP", "address": "'"$BIGIP_TUNNEL_SELFIP"'", "vlan": "openshift_vxlan", "profile": "vxlan-mp", "localAddress": "'"$BIGIP_INT_SELFIP"'", "allowService": "all" }'
 #set allowed services on selfIP of Internal VLAN to be default + tcp:4789
-curl -s -k -X PATCH -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/self/self_3nic -d '{"allowService": ["default","tcp:4789"]}'
+curl -s -k -X PATCH -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/self/tunnelSelfIP -d '{"allowService": ["default","tcp:4789"]}'
 #Create routes on BIG-IP so that traffic to the Master and Worker nodes traverses the internal NIC
 curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/route -d '{"name": "routeMasterNodes", "partition": "Common", "gw": "'"$BIGIP_INT_GW"'", "network": "'"$MASTER_SUBNET_IP_CIDR"'"}'
 curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/route -d '{"name": "routeWorkerNodes", "partition": "Common", "gw": "'"$BIGIP_INT_GW"'", "network": "'"$WORKER_SUBNET_IP_CIDR"'"}'
