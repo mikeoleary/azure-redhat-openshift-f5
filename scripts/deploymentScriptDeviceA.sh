58ba1594669107a0aa8a3fd237ccf1463f8ebd94
Ubuntu <ubuntu@ip-10-0-0-36.ec2.internal>
2020-09-25 00:39:50 +0000

    script work - updated f5.json to use SystemAssignedManagedIdentity - updated installCFE to upload and configure - removed script for deviceB - will consolidate

--- scripts/deploymentScriptDeviceA.sh
+++ scripts/deploymentScriptDeviceA.sh
@@ -8,6 +8,11 @@ BIGIP_VSERVER_ADDR=$4
 BIGIP_INT_FLOAT_IP=$5
 MY_CUSTOM_DOMAIN=$6
 
+## THIS SCRIPT ALSO REQUIRES ENVIRONMENT VARIABLES OF THE FOLLOWING:
+#BIGIP_MGMT_PASSWORD
+#BASE_URL
+#BRANCH
+
 #Get ARO cluster details, and password
 ARO_CREDENTIALS=$(az aro list-credentials --name $ARO_CLUSTER_NAME --resource-group $ARO_CLUSTER_RESOURCE_GROUP)
 ARO_USERNAME=$(echo $ARO_CREDENTIALS | jq .kubeadminUsername -r)
@@ -51,35 +56,45 @@ wget $HOST_SUBNET_DOWNLOAD_URL -O /tmp/f5-bigip-node.yaml
 cp /tmp/f5-bigip-node.yaml /tmp/f5-bigip-node-float.yaml
 sed -i -e "s/<INTERNALSELFIP>/$BIGIP_INT_SELFIP/" /tmp/f5-bigip-node.yaml
 sed -i -e "s/<NODENAME>/f5-bigip-node01/" /tmp/f5-bigip-node.yaml
-oc login $ARO_API_SERVER -u=kubeadmin -p=$ARO_PASSWORD
-oc apply -f /tmp/f5-bigip-node.yaml
+
 sed -i -e "s/<INTERNALSELFIP>/$BIGIP_INT_FLOAT_IP/" /tmp/f5-bigip-node-float.yaml
 sed -i -e "s/<NODENAME>/f5-bigip-float/" /tmp/f5-bigip-node-float.yaml
+
+oc login $ARO_API_SERVER -u=kubeadmin -p=$ARO_PASSWORD
+oc apply -f /tmp/f5-bigip-node.yaml
 oc apply -f /tmp/f5-bigip-node-float.yaml
 rm -f /tmp/f5-bigip-node.yaml
 rm -f /tmp/f5-bigip-node-float.yaml
 
-#get cluster network mask. This is usually /14
+#get cluster network mask. This is usually /14 but we will get it in case it is different
 CLUSTERNETWORK=$(oc get clusternetwork -o json | jq .items[0].network -r)
 read -a strarr <<< $(echo $CLUSTERNETWORK | tr "/" " ")
 CLUSTERNETWORK_MASK=${strarr[1]}
 
 #Calculate a self IP for tunnel on BIGIP. First get assigned host subnet, which is usually a /23 range like 10.130.2.0/23. We will assign .100 from this range, eg, 10.130.2.100
-BIGIP_HOST_SUBNET_WITH_MASK=$(oc get hostsubnet f5-bigip-float -o json | jq .subnet -r)
+BIGIP_HOST_SUBNET_WITH_MASK_FLOAT=$(oc get hostsubnet f5-bigip-float -o json | jq .subnet -r)
+read -a strarr <<< $(echo $BIGIP_HOST_SUBNET_WITH_MASK_FLOAT | tr "/" " ")
+BIGIP_HOST_SUBNET_FLOAT=${strarr[0]}
+BIGIP_TUNNEL_SELFIP_FLOAT="${BIGIP_HOST_SUBNET_FLOAT%.*}.100/$CLUSTERNETWORK_MASK"
+# and for the non-float addr
+BIGIP_HOST_SUBNET_WITH_MASK=$(oc get hostsubnet f5-bigip-node01 -o json | jq .subnet -r)
 read -a strarr <<< $(echo $BIGIP_HOST_SUBNET_WITH_MASK | tr "/" " ")
 BIGIP_HOST_SUBNET=${strarr[0]}
 BIGIP_TUNNEL_SELFIP="${BIGIP_HOST_SUBNET%.*}.100/$CLUSTERNETWORK_MASK"
+
 
 ###Connect to BIG-IP and create tunnel profile, tunnel, selfIP on tunnel, and add vxlan port 4789 to internal self IP. Also, create a route so that the openshift worker and master nodes are reached via BIG-IP internal NIC.
 #Create multipoint vxlan profile
 curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/tunnels/vxlan -d '{"name": "vxlan-mp", "floodingType": "multipoint" }'
 #Create vxlan tunnel based on vxlan profile
-curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/tunnels/tunnel -d '{"name": "openshift_vxlan", "key": "0", "profile": "vxlan-mp", "localAddress": "'"$BIGIP_INT_SELFIP"'" }'
-#Create selfIP on tunnel
-curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/self -d '{"name": "tunnelSelfIP", "address": "'"$BIGIP_TUNNEL_SELFIP"'", "vlan": "openshift_vxlan", "profile": "vxlan-mp", "localAddress": "'"$BIGIP_INT_SELFIP"'", "allowService": "all" }'
+curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/tunnels/tunnel -d '{"name": "openshift_vxlan", "key": "0", "profile": "vxlan-mp", "localAddress": "'"$BIGIP_INT_FLOAT_IP"'", "secondary-address": "'"$BIGIP_INT_SELFIP"'", "traffic-group":"traffic-group-1" }'
+#Create localonly selfIP and a floating selfIP on tunnel
+curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/self -d '{"name": "tunnelSelfIP", "address": "'"$BIGIP_TUNNEL_SELFIP"'", "vlan": "openshift_vxlan", "allowService": "all" }'
+curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/self -d '{"name": "tunnelSelfIP_float", "address": "'"$BIGIP_TUNNEL_SELFIP_FLOAT"'", "vlan": "openshift_vxlan", "traffic-group": "traffic-group-1", "allowService": "all" }'
 #set allowed services on selfIP of Internal VLAN to be default + tcp:4789
-curl -s -k -X PATCH -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/self/tunnelSelfIP -d '{"allowService": ["default","tcp:4789"]}'
+curl -s -k -X PATCH -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/self/self_3nic -d '{"allowService": ["default","tcp:4789"]}'
 #Create routes on BIG-IP so that traffic to the Master and Worker nodes traverses the internal NIC
+sleep 5
 curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/route -d '{"name": "routeMasterNodes", "partition": "Common", "gw": "'"$BIGIP_INT_GW"'", "network": "'"$MASTER_SUBNET_IP_CIDR"'"}'
 curl -s -k -X POST -H "Content-Type: application/json" -u admin:$BIGIP_MGMT_PASSWORD https://$BIGIP_MGMT_ADDRESS/mgmt/tm/net/route -d '{"name": "routeWorkerNodes", "partition": "Common", "gw": "'"$BIGIP_INT_GW"'", "network": "'"$WORKER_SUBNET_IP_CIDR"'"}'
 
--- scripts/installCFE.sh
+++ scripts/installCFE.sh
@@ -1,20 +1,47 @@
 #!/bin/bash
 
 ## THIS SCRIPT WILL INSTALL F5 Cloud Failover Extension. IT REQUIRES ENVIRONMENT VARIABLES OF:
-# BIGIP_MGMT_ADDRESS
+# BIGIP_MGMT_ADDRESS [this can be a single value, or a string of values separated by spaces]
 # BIGIP_MGMT_PASSWORD
 # CFE_DOWNLOAD_URL_PREFIX (eg. https://github.com/F5Networks/f5-cloud-failover-extension/releases/download/)
 # CFE_VERSION
+# BASE_URL
+# BRANCH
+
+
+ARR_BIGIP_MGMT_ADDRESS=($BIGIP_MGMT_ADDRESS)
+CREDS=admin:$BIGIP_MGMT_PASSWORD
 
 #Build download url and download RPM file
 FN=f5-cloud-failover-$CFE_VERSION-0.noarch.rpm
 CFE_DOWNLOAD_URL=$CFE_DOWNLOAD_URL_PREFIX'v'$CFE_VERSION/$FN
 wget $CFE_DOWNLOAD_URL -O /tmp/$FN
 
-CREDS=admin:$BIGIP_MGMT_PASSWORD
-#Upload CFE
-LEN=$(wc -c /tmp/$FN | cut -f 1 -d ' ')
-curl -kvu $CREDS https://$BIGIP_MGMT_ADDRESS/mgmt/shared/file-transfer/uploads/$FN -H 'Content-Type: application/octet-stream' -H "Content-Range: 0-$((LEN - 1))/$LEN" -H "Content-Length: $LEN" -H 'Connection: keep-alive' --data-binary @/tmp/$FN
-#Install CFE
-DATA="{\"operation\":\"INSTALL\",\"packageFilePath\":\"/var/config/rest/downloads/$FN\"}"
-curl -kvu $CREDS "https://$BIGIP_MGMT_ADDRESS/mgmt/shared/iapp/package-management-tasks" -H "Origin: https://$BIGIP_MGMT_ADDRESS" -H 'Content-Type: application/json;charset=UTF-8' --data $DATA
+#Build download url and download CFE declaration
+CONFIG_DOWNLOAD_URL=$BASE_URL$BRANCH/templates/cfe.json
+wget $CONFIG_DOWNLOAD_URL -O /tmp/cfe.json
+
+uploadCFE () {
+    LEN=$(wc -c /tmp/$FN | cut -f 1 -d ' ')
+    curl -kvu $CREDS https://$1/mgmt/shared/file-transfer/uploads/$FN -H 'Content-Type: application/octet-stream' -H "Content-Range: 0-$((LEN - 1))/$LEN" -H "Content-Length: $LEN" -H 'Connection: keep-alive' --data-binary @/tmp/$FN
+}
+installCFE () {
+    DATA="{\"operation\":\"INSTALL\",\"packageFilePath\":\"/var/config/rest/downloads/$FN\"}"
+    curl -kvu $CREDS "https://$1/mgmt/shared/iapp/package-management-tasks" -H "Origin: https://$BIGIP_MGMT_ADDRESS" -H 'Content-Type: application/json;charset=UTF-8' --data $DATA
+}
+configureCFE() {
+    BIGIP_MGMT_PRIVATE_IP_WITH_MASK=$(curl -s -k -H "Content-Type: application/json" -u $CREDS https://$i/mgmt/tm/sys/management-ip | jq .items[0].fullPath -r)
+    read -a strarr <<< $(echo $BIGIP_MGMT_PRIVATE_IP_WITH_MASK | tr "/" " ")
+    BIGIP_MGMT_PRIVATE_IP=${strarr[0]}
+    curl -s -k -X PUT -H "Content-Type: application/json" -u $CREDS https://$i/mgmt/tm/sys/db/config.allow.rfc3927 -d '{"value":"enable"}'
+    curl -s -k -X POST -H "Content-Type: application/json" -u $CREDS https://$i/mgmt/tm/sys/management-route -d '{"name":"metadata-route","network":"169.254.169.254/32", "gateway": "'"$BIGIP_MGMT_PRIVATE_IP"'"}'
+    curl -s -k -X POST -H "Content-Type: application/json" -u $CREDS https://$i/mgmt/shared/cloud-failover/declare -d @/tmp/cfe.json
+}
+
+for i in "${ARR_BIGIP_MGMT_ADDRESS[@]}"
+    do
+    uploadCFE $i
+    installCFE $i
+    sleep 5
+    configureCFE $i
+done
